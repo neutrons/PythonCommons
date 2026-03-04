e48e6277b7a009c4457478535554d2219da306c8
Michael Walsh <68125095+walshmm@users.noreply.github.com>
2026-03-04 11:48:56 -0500

    update version test to hopefully account for real version formats (#4)

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,7 +2,7 @@ from commons import __version__
 
 
 def test_version():
-    assert __version__ == "unknown" or "dev" in __version__ or "0.1.0" in __version__
+    assert __version__ == "unknown" or "dev" in __version__ or "rc" in __version__ or len(__version__.split(".")) >= 2
     #      ^ conda env will default to "unknown" if not set
     #                                  ^ pixi will default to the default tag in pryproject.toml
     #                                    if not set, it will default to "0.0.1" + "devxxx" on local dev builds
